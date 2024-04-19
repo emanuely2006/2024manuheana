48dc23603c3343888d5d731d00c0aa2e080299cf
emanuely2006 <127766989+emanuely2006@users.noreply.github.com>
2024-04-19 10:50:34 -0300

    19/04/2024-slad18-codigo8

---
+++ main.js
@@ -0,0 +1,36 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1) { 
+    //tamanhoSenha = tamanhoSenha - 1;
+    tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+    //tamanhoSenha = tamanhoSenha + 1;
+    tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+// código omitido
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+function geraSenha(){
+for (let i = 0; i < tamanhoSenha;i++) {
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    console.log(letrasMaiusculas[numeroAleatorio]);
+   }
+campoSenha.value = letrasMaiusculas;
+}
