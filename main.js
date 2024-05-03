99ed2f4fa4a459a7208c50c2e3189716e6a0dcb3
emanuely2006 <127766989+emanuely2006@users.noreply.github.com>
2024-05-03 10:49:29 -0300

    03 maio- 10 codigo/

--- main.js
+++ main.js
@@ -2,36 +2,63 @@ const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ'; 
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz'; 
+const numeros = '0123456789'; 
+const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho(){
     if (tamanhoSenha > 1) { 
-    //tamanhoSenha = tamanhoSenha - 1;
+    //tamanhoSenha = tamanhoSenha-1;
     tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha
+    geraSenha();
 }
 function aumentaTamanho() {
     if (tamanhoSenha < 20) {
-    //tamanhoSenha = tamanhoSenha + 1;
+    //tamanhoSenha = tamanhoSenha+1;
     tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha()
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+for(i=0; < checkbox.length; i ++){
+    checkbox[i].onclick=geraSenha; 
 }
 
 
-const campoSenha = document.querySelector('#campo-senha');
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
 
 function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    console.log(alfabeto);
+    let senha = '';
 for (let i = 0; i < tamanhoSenha;i++) {
     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
-    console.log(letrasMaiusculas[numeroAleatorio]);
+    senha = senha + alfabeto[numeroAleatorio];
    }
-campoSenha.value = letrasMaiusculas;
+campoSenha.value = senha;
 }
